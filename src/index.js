a26c9665d68567d24afb9add243f7950b4135fc2
Alex Modin <almod90@gmail.com>
2019-10-30 17:27:34 +0400

    finished

--- src/index.js
+++ src/index.js
@@ -3,32 +3,38 @@ import axios from 'axios'
 import $ from 'jquery'
 import './js/api'
 
-const $button = $('#action');
+const $button = $('#action'),
+    $table = $('#table');
+
+
+const operate = d => {
+    const total = d.map(e => e.traffic).reduce((a, b) => a + b);
+    console.log(`Total: ${total}`);
+
+    const newInstance = d.map(e => {
+        e.percent = Math.ceil(100.0 * e.traffic / total);
+        return e;
+    });
+    console.log(newInstance);
+
+    const filtered = newInstance.filter(e => e.percent > 2);
+    console.log(filtered);
+
+    const start = '<div class="text-center pt-2 h4">Details</div> <table><tr><th>Name</th><th>Traffic</th><th>Percent</th></tr>',
+        end = '</table><div class="text-center text-muted small pt-2">check console for more info</div>';
+    let content = "";
+
+    newInstance.forEach(e => content += `<tr><td>${e.name}</td><td>${e.traffic}</td><td>${e.percent} %</td></tr>`);
+
+    $table.html(start + content + end);
+}
 
 $button.click(function (e) {
     e.preventDefault();
 
-    const object = {
-        name: "Alex",
-        surname: "Modin",
-        email: "almod90@gmail.com",
-        skills: ["PHP", "JavaScript", "Linux"]
-    };
-
-    const data = {
-        json: JSON.stringify(object)
-    };
-
-    axios.post('api/test-2', data)
+    // not using fetch because mock won't handle it
+    axios.get('/api/test-3')
         .then(r => r.data)
-        .then(d => {
-            if (d.success) {
-                alert('JSON was send and it\'s valid!');
-            } else {
-                //console.log(d.error);
-                throw new Error(d.error);
-            }
-        })
+        .then(d => operate(d))
         .catch(e => console.log(`Error: ${e.message}`))
-
 });
--- src/js/api.js
+++ src/js/api.js
@@ -3,27 +3,25 @@ const MockAdapter = require('axios-mock-adapter');
 
 const mock = new MockAdapter(axios);
 
-mock.onPost('/api/test-2').reply(function (config) {
-    const data = JSON.parse(config.data);
-
-    try {
-        JSON.parse(data.json);
+mock.onGet('/api/test-3').reply(200, [
+    {
+        name: "object 1",
+        traffic: 1231
+    },
+    {
+        name: "object 2",
+        traffic: 2345
+    },
+    {
+        name: "object 3",
+        traffic: 94565
+    },
+    {
+        name: "object 4",
+        traffic: 14512
+    },
+    {
+        name: "object 5",
+        traffic: 53421
     }
-    catch (e) {
-        response(e.message)
-    }
-    return response();
-});
-
-const response = function (error = "") {
-    if (error.length === 0) {
-        return [200, {
-            success: true
-        }];
-    } else {
-        return [200, {
-            success: false,
-            error: error
-        }];
-    }
-};
+]);
